86eef7c46fa163a55af8b5c62db829b33f23696b
Kurt Edelbrock <kurtedelbrock@gmail.com>
2012-06-08 22:44:01 -0400

    Implemented blank screen for showing a task

---
+++ Philosophy/View Controllers/PLShowTaskViewController.h
@@ -0,0 +1,13 @@
+//
+//  PLShowTaskViewController.h
+//  Philosophy
+//
+//  Created by Jenny Zhang on 6/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PLShowTaskViewController : UIViewController
+
+@end
---
+++ Philosophy/View Controllers/PLShowTaskViewController.m
@@ -0,0 +1,43 @@
+//
+//  PLShowTaskViewController.m
+//  Philosophy
+//
+//  Created by Jenny Zhang on 6/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PLShowTaskViewController.h"
+
+@interface PLShowTaskViewController ()
+
+@end
+
+@implementation PLShowTaskViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end

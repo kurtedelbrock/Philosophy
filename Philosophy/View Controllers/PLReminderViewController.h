e05a7f1ac986f81794406af78c1f1fb71f02e9d3
Kurt Edelbrock <kurtedelbrock@gmail.com>
2012-06-11 12:26:12 -0400

    Added view controller and interface design for the reminder screen

---
+++ Philosophy/View Controllers/PLReminderViewController.h
@@ -0,0 +1,13 @@
+//
+//  PLReminderViewController.h
+//  Philosophy
+//
+//  Created by Kurt Edelbrock on 6/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PLReminderViewController : UITableViewController
+
+@end

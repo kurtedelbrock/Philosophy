2984e8fe65cfc8dffc041a8996910922ec10e791
Kurt Edelbrock <kurtedelbrock@gmail.com>
2012-06-11 12:43:26 -0400

    Slight UI hacks

--- Philosophy/View Controllers/PLReminderViewController.m
+++ Philosophy/View Controllers/PLReminderViewController.m
@@ -28,6 +28,7 @@
     [super viewDidLoad];
     
     self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"dark_denim.png"]];
+    
 
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
